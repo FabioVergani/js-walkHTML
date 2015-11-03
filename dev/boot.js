8f1535b4eed70ed17b9c317e6ec64dca002a4368
Fabio Vergani <fabio.vergani.1975@gmail.com>
2015-11-03 13:25:24 +0100

    Create boot.js

---
+++ dev/boot.js
@@ -0,0 +1,14 @@
+(function(global){'use strict';
+ function binds(o,p){var e=o;return e[p].bind(e);}
+ var undefined, w=global, d=w.document, df=d.createDocumentFragment(),
+ f=binds,
+ e,
+ console=e=w.console, log=f(e,'log'), dir=f(e,'dir'),
+ createText=f(d,'createTextNode'),
+ createTag=f(d,'createElement');
+
+ e=f=null;
+
+
+})(window);
+//
