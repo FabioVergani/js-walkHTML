9cfe06b3b00aa4c6efbfd1b4ceecfad9c7de7876
Fabio Vergani <fabio.vergani.1975@gmail.com>
2015-11-04 17:17:47 +0100

    Update walkHTML.js

--- walkHTML.js
+++ walkHTML.js
@@ -1,37 +1,30 @@
-var log=(function(o){return o.log.bind(o);})(console);
-//=================================================================================
-//# i:depth, j:childnum, e:elementNode
+//# t:total, i:depth, j:childnum, e:elementNode
 function process(t,i,j,e){
- log((("000"+t).slice(-3))+'\t',Array(i).join('\t'),i,j,e.tagName);
+ var s='\t';
+ console.log((("000"+t).slice(-3))+s,Array(i).join(s),Number(i+'.'+j),e.tagName);
 };
-//=================================================================================
-
+//=====================================================================================
 
 function walkHTML(from,handle){
-	var e=from, f=handle, k=false, i=0, j=0, t=0, x;
-	do{
-		t++;
-		f(t,i,j,e);
-		x=e.firstElementChild;
-		if(k===false && x!==null){
-			j=0;
-			i++;
-		}else{
-			k=false;
-			x=e.nextElementSibling;
-			if(x===null){x=e.parentElement;k=true;j=0;i--;};
-		};
-		j++;
-		e=x;
-	}while(i>0);
+ var e=from, f=handle, k=false, i=0, j=0, t=0, n;
+ do{
+	if(k===false){f(t,i,j,e)};
+	n=e.firstElementChild;
+	if(k===false && n!==null){
+	 j=0;
+	 i++;
+	}else{
+	 k=false;
+	 n=e.nextElementSibling;
+	 if(n===null){
+		n=e.parentElement;
+		k=true;
+		j=0;
+		i--;
+	 };
+	};
+	j++;
+	t++;
+	e=n;
+ }while(i>0);
 }
-
-
-
-//
-//
-/*console.log('§§§§§§§§§§§');
-				//if(n.tagName==='BODY'){f(t,i,j,e);break};
-file:///G:/SVILUPPO/WALK/1/walkHTML.htm
-
-*/
