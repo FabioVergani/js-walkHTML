37e8ec9565ea311a83c3536a8397d4864436387b
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-03-29 14:18:10 +0200

    Update run.js

--- dumper/run.js
+++ dumper/run.js
@@ -1,28 +1,25 @@
 function process(node){//counts, depth, childindex,
- var e=node, s=e.id;
- console.log((new Array(e.depth)).join(' '),e.childIndex,e.tagName,s?'#'+s:'');
+ var e=node, s=e.id, hasChildren=e.children.length!==0;
+ console.log((new Array(e.depth)).join(' '),e.childIndex,e.tagName,s?'#'+s:'',hasChildren);
 }
-
 //
 
 function walkTree(process,html){
  var j, i, f=process, e=html, n=e.ownerDocument, o=n.createTreeWalker(e,1,null,false);
  e.childIndex=0;
  e.depth=0;
- do{
-	f(e);
-	if((e=o.nextNode())!==null){
-		n=e.previousElementSibling;
-		if(n===null){
-			i=0;
-			j=e.parentElement.depth+1;
-		}else{
-			i=n.childIndex+1;
-			j=n.depth;
-		};
-		e.childIndex=i;
-		e.depth=j;
+ f(e);
+ while((e=o.nextNode())!==null){
+	n=e.previousElementSibling;
+	if(n===null){
+		i=0;
+		j=e.parentElement.depth+1;
+	}else{
+		i=n.childIndex+1;
+		j=n.depth;
 	};
- }while(e!==null);
+	e.childIndex=i;
+	e.depth=j;
+	f(e);
+ };
 }
-
