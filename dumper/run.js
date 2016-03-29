25c0a0b4877e78862e53d5ad0f08f3c8e33acc8d
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-03-29 16:23:31 +0200

    Update run.js

--- dumper/run.js
+++ dumper/run.js
@@ -1,27 +1,45 @@
-function process(node){//counts, depth, childindex,
- var e=node, s=e.id, hasChildren=e.children.length!==0;
- console.log((new Array(e.depth)).join(' '),e.childIndex,e.tagName,s?'#'+s:'',hasChildren?'descend':'');
-}
-//
+
+var Path=[],lastPath;
+
 
 //#
+//#
 function walkTree(process,html){
- var j, i, f=process, e=html, n=e.ownerDocument, o=n.createTreeWalker(e,1,null,false);
+ var c, p, j, i, f=process, e=html, n=e.ownerDocument, o=n.createTreeWalker(e,1,null,false);
+ e.childCountedTypes={};
  e.childIndex=0;
  e.depth=0;
  f(e);
  while((e=o.nextNode())!==null){
+	p=e.parentElement;
 	n=e.previousElementSibling;
 	if(n===null){
 		i=0;
-		j=e.parentElement.depth+1;
+		j=p.depth+1;
 	}else{
 		i=n.childIndex+1;
 		j=n.depth;
+	};
+	c=p.childCountedTypes;
+	n=e.tagName;
+	if(typeof(c)==='undefined'){
+		(c=p.childCountedTypes={})[n]=1;
+	}else{
+		if(n in c){c[n]++;}else{c[n]=1;};
 	};
 	e.childIndex=i;
 	e.depth=j;
 	f(e);
  };
 }
+
+//#
+function process(node){//counts, depth, childindex,
+ var m=Path, e=node, i=e.depth ;
+ //m.length=i;
+ //m[i]=[e.tagName];
+ //console.log(m.join(' > ')+':nth-child()');
+	console.log(i,e.tagName)
+}
+
 
